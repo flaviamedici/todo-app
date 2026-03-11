f0449e90b6dc417d97570d7b6efbbfda58348ad8
flaviamedici <flaviaomedici@gmail.com>
2026-03-10 17:45:07 -0700

    Update for bonus_16.py, created a function to take files and compress them to a folder

--- Bonus_Practices/bonus16.py
+++ Bonus_Practices/bonus16.py
@@ -1,12 +1,14 @@
 import FreeSimpleGUI as sg
+from zip_creator import make_archive
+
 
 label1 = sg.Text("Select files to compress: ")
 input1 = sg.Input()
-choose_button = sg.FilesBrowse('Choose')
+choose_button = sg.FilesBrowse('Choose', key='files')
 
 label2 = sg.Text("Select folder destination: ")
 input2 = sg.Input()
-choose_folder_button = sg.FolderBrowse('Choose')
+choose_folder_button = sg.FolderBrowse('Choose', key='folder')
 
 compress_button = sg.Button('Compress')
 
@@ -15,5 +17,11 @@ window = sg.Window("File Compressor", layout=[
     [label2, input2, choose_folder_button],
     [compress_button]])
 
-window.read()
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values['files'].split(';')
+    folder = values['folder']
+    make_archive(filepaths, folder)
+
 window.close()
