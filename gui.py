dee6ea2f815efc5db49cab31447ee212cbd61aaf
flaviamedici <flaviaomedici@gmail.com>
2026-03-13 16:46:30 -0700

    Complete and exit buttons

--- gui.py
+++ gui.py
@@ -6,8 +6,14 @@ input_box = gui.InputText(tooltip="Enter todo", key="todo")
 add_button = gui.Button("Add")
 edit_button = gui.Button("Edit")
 list_box = gui.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=(40, 10))
+complete_button = gui.Button("Complete")
+exit_button = gui.Button("Exit")
 
-layout = [[label], [input_box, add_button], [list_box, edit_button]]
+
+layout = [[label],
+          [input_box, add_button],
+          [list_box, edit_button, complete_button],
+          [exit_button]]
 window = gui.Window('To-Do app', layout=layout,
                     font=("Helvetica", 20))
 
@@ -31,8 +37,17 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
         case 'todos':
             window['todo'].update(value=values['todos'][0])
+        case "Exit":
+            break
         case gui.WIN_CLOSED:
             break
 
