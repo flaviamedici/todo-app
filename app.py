8ca0a4495aae33b69c61df5ffa3ef3e087d19e9c
flaviamedici <flaviaomedici@gmail.com>
2026-03-14 17:28:53 -0700

    Complete and exit buttons

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from functions import *
+import functions
 import time
 
 now = time.strftime("%c")
@@ -12,15 +12,15 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
         
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo + '\n')
 
-        write_todos(todos)
+        functions.write_todos(todos)
 
 
     elif user_action.startswith("show"):
-        todos = get_todos()
+        todos = functions.get_todos()
 
         # new_todos = []
         # for item in todos:
@@ -40,12 +40,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input("Enter a new todo: ")
             todos[number] = new_todo + '\n'
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
         except ValueError:
             print("Your command is not valid")
@@ -55,13 +55,13 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list"
             print(message)
--- gui.py
+++ gui.py
@@ -1,16 +1,20 @@
 import functions
 import FreeSimpleGUI as gui
+import time
 
+gui.theme('DarkBlue')
+clock = gui.Text('', key='clock')
 label = gui.Text("Type a todo: ")
 input_box = gui.InputText(tooltip="Enter todo", key="todo")
-add_button = gui.Button("Add")
+add_button = gui.Button("Add", size=10)
 edit_button = gui.Button("Edit")
-list_box = gui.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=(40, 10))
+list_box = gui.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=(40, 12))
 complete_button = gui.Button("Complete")
 exit_button = gui.Button("Exit")
 
 
-layout = [[label],
+layout = [[clock],
+          [label],
           [input_box, add_button],
           [list_box, edit_button, complete_button],
           [exit_button]]
@@ -18,7 +22,8 @@ window = gui.Window('To-Do app', layout=layout,
                     font=("Helvetica", 20))
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=2000)
+    window['clock'].update(value=time.strftime("%c"))
     print(event)
     print(values)
     match event:
@@ -28,22 +33,29 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
-        case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
-
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-        case "Complete":
-            todo_to_complete = values['todos'][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
             window['todo'].update(value="")
+        case "Edit":
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
+
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                gui.popup("Please select an item first", font=("Helvetica", 20))
+        case "Complete":
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value="")
+            except IndexError:
+                gui.popup("Please select an item first", font=("Helvetica", 20))
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case "Exit":
